776a5aa627c5f883910d9b802428315d79d3ce12
LCoding1 <95256919+LCoding1@users.noreply.github.com>
2021-11-29 18:52:14 -0500

    Update password gen.py

--- password gen.py
+++ password gen.py
@@ -7,4 +7,4 @@ Symbol = "[]{}#()<>*;_-.,\|~`"
 all=lower + upper + NUMBER + Symbol
 length = 16
 password = "".join(random.sample(all,length))
-print (" The Password You Generated is",password)
+print ("Password:",password)
